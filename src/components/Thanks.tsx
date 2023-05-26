d9a47dc15f2c33959896fc1a440418234be0dd6d
tomashoj2001 <tomashojnadel@gmail.com>
2023-05-26 14:26:37 -0300

    fix: image position

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -7,11 +7,11 @@ interface ThanksProps {
 
 export default function Thanks({ email, setHome }: ThanksProps) {
   return (
-    <div className="px-8 py-1 h-[99vh] md:h-auto md:max-w-[400px] md:p-8 md:mt-40">
+    <div className="px-8 py-1 h-[99vh] md:h-auto md:max-w-[400px] md:p-8">
       <picture>
         <img src={img} alt="foto" className="mt-16 md:mt-0" />
       </picture>
-      <h1 className="text-4xl font-bold mt-12 mb-8 text-dark-blue md:mt-0">Thanks for subscribing!</h1>
+      <h1 className="text-4xl font-bold mt-12 mb-8 text-dark-blue md:mt-8">Thanks for subscribing!</h1>
       <p>A confirmation email has been sent to <span className="font-semibold">{email}</span>. Please open it and click the button inside to confirm your subscription</p>
       <button
         className="text-white py-4 rounded w-full mt-32 bg-dark-blue md:mt-8 md:hover:bg-tomato"
